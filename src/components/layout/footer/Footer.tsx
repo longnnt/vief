87a8bfd7faadf3fd332b0c30ac9e346d7eb0beca
Song Nguyen <86753454+songnn1699@users.noreply.github.com>
2022-08-29 11:31:16 +0700

    fix router

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,16 +1,22 @@
 import { WebContainer } from "@/src/common/components/WebContainer";
+import { ROUTE_ABOUT, ROUTE_ENTERPRISE_STORY, ROUTE_EVENT, ROUTE_HOME, ROUTE_LIBRARY, ROUTE_POLICY } from "@/src/common/constants/routes.constant";
+import { useViefRouter } from "@/src/common/hooks/useViefRouter";
 import { Box, Center, Link, SimpleGrid, Stack, Text } from "@chakra-ui/react";
 import { ReactNode } from "react";
 
 const ListHeader = ({ children }: { children: ReactNode }) => {
   return (
-    <Text fontSize="20px" fontWeight={"600"}>
+    <Text variant='text20'>
       {children}
     </Text>
   );
 };
 
 const Footer = () => {
+  const router= useViefRouter()
+  const handleRouter=(children:any) =>{
+    router.push(children)
+  }
   return (
     <Box bg="#F2F3F7" w="100%">
       <Center>
@@ -35,42 +41,42 @@ const Footer = () => {
 
             <Stack align={"flex-start"} spacing="8px">
               <ListHeader>Địa chỉ</ListHeader>
-              <Text fontSize="14px" fontWeight={"500"}>
+              <Text variant='text14'>
                 Điện thoại: 024 2239 7555
               </Text>
-              <Text fontSize="14px" fontWeight={"500"}>
+              <Text variant='text14'>
                 Email: info@vief.vn
               </Text>
-              <Text fontSize="14px" fontWeight={"500"}>
+              <Text variant='text14'>
                 Điện thoại: 024 2239 7555
               </Text>
-              <Text fontSize="14px" fontWeight={"500"}>
+              <Text variant='text14'>
                 Email: info@vief.vn
               </Text>
             </Stack>
             <Stack align={"flex-start"} spacing="8px">
               <ListHeader>Điều hướng</ListHeader>
-              <Link href="/" fontSize="14px" fontWeight={"500"}>
+              <Text onClick={() => handleRouter(ROUTE_HOME.en)} variant='text14' cursor="pointer">
                 Trang chủ
-              </Link>
-              <Link href="/policy" fontSize="14px" fontWeight={"500"}>
+              </Text>
+              <Text onClick={() => handleRouter(ROUTE_POLICY.en)} variant='text14' cursor="pointer">
                 Chính sách
-              </Link>
-              <Link href="/events" fontSize="14px" fontWeight={"500"}>
+              </Text>
+              <Text onClick={() => handleRouter(ROUTE_EVENT.en)} variant='text14' cursor="pointer">
                 Sự kiện
-              </Link>
-              <Link href="/enterprise-story" fontSize="14px" fontWeight={"500"}>
+              </Text>
+              <Text onClick={() => handleRouter(ROUTE_ENTERPRISE_STORY.en)} variant='text14' cursor="pointer">
                 Doanh nghiệp
-              </Link>
-              <Link href="/library" fontSize="14px" fontWeight={"500"}>
+              </Text>
+              <Text onClick={() => handleRouter(ROUTE_LIBRARY.en)} variant='text14' cursor="pointer">
                 Thư viện
-              </Link>
-              <Link href="/about" fontSize="14px" fontWeight={"500"}>
+              </Text>
+              <Text onClick={() => handleRouter(ROUTE_ABOUT.en)} variant='text14' cursor="pointer">
                 Về chúng tôi
-              </Link>
-              <Link href="/about" fontSize="14px" fontWeight={"500"}>
+              </Text>
+              <Text onClick={() => handleRouter(ROUTE_ABOUT.en)} variant='text14' cursor="pointer">
                 Liên hệ
-              </Link>
+              </Text>
             </Stack>
           </SimpleGrid>
         </WebContainer>
